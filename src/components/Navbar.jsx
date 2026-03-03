5ff3f4c720baf2612c5652246be26d3489ad5266
nnadra <nailanadrafahira@gmail.com>
2026-03-03 13:30:29 +0700

    struktur folder dan add react router doom

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,9 @@
+const Navbar = () => {
+  return (
+    <div>
+      <h1 className='m-8 bg-amber-300'>Navbar</h1>
+    </div>
+  )
+}
+
+export default Navbar
---
+++ src/pages/LandingPage/index.jsx
@@ -0,0 +1,7 @@
+export default function index() {
+  return (
+    <div>
+      <h1 className=''>ini home</h1>
+    </div>
+  )
+}
